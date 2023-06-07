ac8dfcba846b5e76d3500646f19d3956e2006be7
yogesh <hemant.kaushik@writlabs.com>
2023-06-07 16:43:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState("");
+  const [newData, setNewData] = useState([]);
+
+  // closure started
+  // function outerFunction() {
+  //   var outerVariable = "I am from the outer function";
+
+  //   function innerFunction() {
+  //     console.log(outerVariable);
+  //   }
+
+  //   return innerFunction;
+  // }
+
+  // var closure = outerFunction(); // Assigning the returned inner function to a variable
+
+  // closure();
+  // closure ended
+
+  // console.log(a, "a");
+  // console.log(b, "b");
+  // console.log(c, "c");
+
+  // const a = 2;
+  // let b = 3;
+  // var c = 4;
+
+  const handleTodo = () => {
+    if (data.trim() === "") {
+      alert("enter todo");
+    } else {
+      setNewData([...newData, data]);
+      setData("");
+    }
+  };
+
+  const handleDelete = (i) => {
+    const updatedData = [...newData];
+    updatedData.splice(i, 1);
+    setNewData(updatedData);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={data}
+        onChange={(e) => setData(e.target.value)}
+        type="text"
+      />
+      <button onClick={handleTodo}>Add</button>
+      <div>
+        {newData.map((todo, id) => {
+          return (
+            <div>
+              <li key={todo.id}>
+                {todo}
+                <button onClick={() => handleDelete(id)}>Delete</button>
+              </li>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
